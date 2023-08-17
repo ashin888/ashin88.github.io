f005f5f64b678af5d03b7f6906506048cf07f5fa
Ashin <94775483+ashin888@users.noreply.github.com>
2023-08-17 17:56:24 +0530

    Update script.js

--- short/script.js
+++ short/script.js
@@ -1,18 +1,21 @@
-function generateShortUrl() {
-    const originalUrl = document.getElementById('originalUrl').value;
-    if (!originalUrl) {
-        alert('Please enter a URL');
-        return;
-    }
+function shortenUrl() {
+  const longUrl = document.getElementById("longUrl").value;
+  
+  // Generate a random 4-character alphanumeric string
+  const shortCode = generateShortCode();
 
-    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let shortCode = '';
-    for (let i = 0; i < 4; i++) {
-        const randomIndex = Math.floor(Math.random() * randomChars.length);
-        shortCode += randomChars.charAt(randomIndex);
-    }
+  // Construct the short URL
+  const shortUrl = "https://ashin888.github.io/short/" + shortCode;
+  
+  document.getElementById("shortUrl").textContent = "Short URL: " + shortUrl;
+}
 
-    const shortUrl = 'https://ashin888.github.io/short/' + shortCode;
-    document.getElementById('shortUrl').textContent = shortUrl;
-    document.getElementById('shortUrl').setAttribute('href', shortUrl);
+function generateShortCode() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let shortCode = "";
+  for (let i = 0; i < 4; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    shortCode += characters.charAt(randomIndex);
+  }
+  return shortCode;
 }
