143f8fc0196f4cb2546c243e5b7ec1dfc323972c
Ashin <94775483+ashin888@users.noreply.github.com>
2023-08-17 17:28:46 +0530

    Update script.js

--- short/script.js
+++ short/script.js
@@ -1,21 +1,38 @@
-document.getElementById('generateButton').addEventListener('click', function () {
-  const longUrl = document.getElementById('longUrl').value;
-  if (longUrl) {
-    const shortUrl = generateShortUrl();
-    const shortUrlContainer = document.getElementById('shortUrlContainer');
-    shortUrlContainer.innerHTML = `<p>Short URL: <a href="${shortUrl}" target="_blank">${shortUrl}</a></p>`;
-    setTimeout(function () {
-      window.location.href = longUrl;
-    }, 2000);
-  }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const generateButton = document.getElementById("generateButton");
+    const longUrlInput = document.getElementById("longUrl");
+    const shortUrlLink = document.getElementById("shortUrl");
 
-function generateShortUrl() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let shortUrl = '';
-  for (let i = 0; i < 4; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    shortUrl += characters.charAt(randomIndex);
-  }
-  return `http://ashin888.github.io/short/${shortUrl}`;
-}
+    generateButton.addEventListener("click", function() {
+        const longUrl = longUrlInput.value;
+        if (longUrl === "") {
+            alert("Please enter a valid long URL.");
+            return;
+        }
+
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let shortUrl = "";
+        for (let i = 0; i < 4; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            shortUrl += characters.charAt(randomIndex);
+        }
+
+        const shortUrlPageContent = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta http-equiv="refresh" content="0;url=${longUrl}">
+            </head>
+            <body>
+                Redirecting...
+            </body>
+            </html>
+        `;
+
+        const shortUrlBlob = new Blob([shortUrlPageContent], { type: "text/html" });
+        const url = URL.createObjectURL(shortUrlBlob);
+
+        shortUrlLink.href = url;
+        shortUrlLink.textContent = url;
+    });
+});
