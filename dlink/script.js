d64507961d746d742bac2be59160cfa8bec8e4be
Ashin <94775483+ashin888@users.noreply.github.com>
2023-08-17 18:42:59 +0530

    Update script.js

--- dlink/script.js
+++ dlink/script.js
@@ -1,17 +1,16 @@
-// script.js
 function convertLink() {
-    var driveLink = document.getElementById("driveLink").value;
-    var fileId = driveLink.match(/\/d\/(.+?)\/view/)[1];
-    var directDownloadLink = "https://drive.google.com/uc?export=download&id=" + fileId;
-    document.getElementById("output").innerHTML = "<p>Direct Download Link:</p><a href='" + directDownloadLink + "'>" + directDownloadLink + "</a> <button id='copyButton' onclick='copyLink()'>Copy</button>";
-}
+            var driveLink = document.getElementById("driveLink").value;
+            var fileId = driveLink.match(/\/d\/(.+?)\/view/)[1];
+            var directDownloadLink = "https://drive.google.com/uc?export=download&id=" + fileId;
+            document.getElementById("output").innerHTML = `<p>Direct Download Link:</p><a href="${directDownloadLink}">${directDownloadLink}</a> <button id="copyButton" onclick="copyLink()">Copy</button>`;
+        }
 
-function copyLink() {
-    var copyText = document.createElement("input");
-    copyText.value = document.querySelector("#output a").href;
-    document.body.appendChild(copyText);
-    copyText.select();
-    document.execCommand("copy");
-    document.body.removeChild(copyText);
-    alert("Link copied to clipboard");
-}
+        function copyLink() {
+            var copyText = document.createElement("input");
+            copyText.value = document.querySelector("#output a").href;
+            document.body.appendChild(copyText);
+            copyText.select();
+            document.execCommand("copy");
+            document.body.removeChild(copyText);
+            alert("Link copied to clipboard");
+        }
